069eac4aad6bf8b565b0096754e5675fc6b1713b
Aditya Upadhyay <aadit0402@gmail.com>
2018-04-01 13:10:44 +0530

    Header files added here.

---
+++ include/gdt.h
@@ -0,0 +1,39 @@
+#ifndef __GDT_H
+#define __GDT_H
+
+#include <common/types.h>
+namespace myos
+{
+    class GlobalDescriptorTable
+    {
+        public:
+            class SegmentDescriptor
+            {
+            private:
+                myos::common::uint16_t limit_lo;
+                myos::common::uint16_t base_lo;
+                myos::common::uint8_t base_hi;
+                myos::common::uint8_t type;
+                myos::common::uint8_t flags_limit_hi;
+                myos::common::uint8_t base_vhi;
+            public:
+                SegmentDescriptor(myos::common::uint32_t base,  myos::common::uint32_t limit, myos::common::uint8_t type);
+                myos::common::uint32_t Base();
+                myos::common::uint32_t Limit(); 
+            } __attribute__((packed));
+
+    private:
+            SegmentDescriptor nullSegmentSelector;
+            SegmentDescriptor unusedSegmentSelector;
+            SegmentDescriptor codeSegmentSelector;
+            SegmentDescriptor dataSegmentSelector;
+            
+    public:
+        GlobalDescriptorTable();
+        ~GlobalDescriptorTable();
+        
+        myos::common::uint16_t CodeSegmentSelector();//give offset for code Segment 
+        myos::common::uint16_t DataSegmentSelector(); // 
+    };
+}
+#endif
